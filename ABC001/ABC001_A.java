bab396e3718192f3c955ab061c8a15390d0f8131
齋藤陸弥 <Rikuya@Rikuya-Mac.local>
2016-02-19 23:27:22 +0900

    ABC001_A

---
+++ ABC001/ABC001_A.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		int H1,H2;
+		Scanner sc = new Scanner(System.in);
+		H1 = sc.nextInt();
+		H2 = sc.nextInt();
+		System.out.println(H1 - H2);
+	}
+}
