4affaa5f387392c54ffabc8c9af1edf00027dbce
Rikuya6 <rikuya6@gmail.com>
2017-02-19 23:07:01 +0900

    0121 TLE

--- AOJ/0121.cpp
+++ AOJ/0121.cpp
@@ -1,78 +1,110 @@
-// TLEする
-
 #include <iostream>
-#include <string>
-#include <vector>
-#include <set>
 #include <queue>
 #include <algorithm>
+#include <cstring>
+#include <map>
 
 using namespace std;
 #define REP(i, j) for(int i = 0; i < j; i++)
 
-int solve(const string &start)
+int encode(const int data[])
 {
-  string goal = "01234567";
-  deque<string> open; // 状態
-  deque<int> count_que;
-  set<string> closed;
+  int e = 0;
+  REP(i, 8)
+    e |= data[i] << (3 * i);
+  return e;
+}
 
-  open.push_back(start);
-  closed.insert(start);
-  count_que.push_back(0);
+void decode(const int e, int d[])
+{
+  for(int i = 7; i >= 0; i--)
+    d[i] = ((e >> (3 * i)) & 7);
+  return;
+}
+
+void bit_debug(const int e)
+{
+  cout << "bit: ";
+  REP(i, 10){
+    cout << ((e >> (3 * i)) & 7) << " ";
+  }
+  cout << endl;
+}
+
+
+
+int solve(const int start)
+{
+  int end_str[] = {0, 1, 2, 3, 4, 5, 6, 7};
+  int end = encode(end_str);
+  queue<int> open, cnt_que;
+  map<int, int> closed;
+
+  open.push(start);
+  closed[start] = 1;
+  cnt_que.push(0);
   while(! open.empty()){
-    const string state = open.front();
-    const int count = count_que.front();
-    open.pop_front(); count_que.pop_front();
+    int state = open.front(); open.pop();
+    int cnt = cnt_que.front(); cnt_que.pop();
+    int data[8];
+    int zeroX, zeroY;
 
-    if(state == goal) return count;
+    if(state == end) return cnt;
 
-    // 次の変化
-    vector< vector<int> > next_pos{ {1, 4}, {-1, 1, 4}, {-1, 1, 4}, {-1, 4}, {-4, 1}, {-1, 1, -4}, {-1, 1, -4}, {-1, -4} };
-    string nstate;
-    int zero_pos = state.find("0");
-    // cout << "index:"<< index << endl;
-    // REP(k, 8){
-    //   cout << state[k];
-    // }
-    // cout << endl;
-    REP(i, 8) {
-      if(zero_pos == i){
-        for(auto it = next_pos[i].begin(); it != next_pos[i].end(); ++it){
-          nstate = state;
-          swap(nstate[zero_pos], nstate[zero_pos + *it]);
-          // REP(k, 8){
-          //   cout << state[k];
-          // }
-          // cout << endl;
-          // REP(k, 8){
-          //   cout << nstate[k];
-          // }
-          // cout << endl;
-          if(closed.find(nstate) == closed.end()){
-            open.push_back(nstate);
-            count_que.push_back(count + 1);
-            closed.insert(nstate);
-          }
-        } // for
+    decode(state, data);
+    REP(i, 8){
+      if(data[i] == 0){
+        zeroX = i % 4;
+        zeroY = i / 4;
         break;
-      } // if
-    } // REP
-  } // while
+      }
+    }
+    // cout << "x: " << zeroX << " y: " << zeroY << endl;
+
+    int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+    int ndata[8], nstate;
+    REP(d, 4){
+      int nx = zeroX + dx[d];
+      int ny = zeroY + dy[d];
+
+      if(nx < 0 || nx > 7) continue;
+      if(ny < 0 || ny > 1) continue;
+      if(ny == 0 && nx > 3) continue;
+      if(ny == 1 && nx < 0) continue;
+
+      REP(i, 8)
+        ndata[i] = data[i];
+      // cout << "zy: " << zeroY << " zx: " << zeroX << endl;
+      // cout << "ny: " << ny << " nx: " << nx << endl;
+      // cout << ndata[zeroY * 4 + zeroX] <<  " " << ndata[ny * 4 + nx] << endl;
+      swap(ndata[zeroY * 4 + zeroX], ndata[ny * 4 + nx]);
+      nstate = encode(ndata);
+      // cout << nstate << endl;
+      if(! closed.count(nstate)){
+        open.push(nstate);
+        closed[nstate] = 1;
+        // cout << "cnt: " << cnt << endl;
+        cnt_que.push(cnt + 1);
+      }
+    }
+  }
   return -1;
 }
 
 int main()
 {
+  ios::sync_with_stdio(false);
+
   int num, ans;
+  int start[8];
   while(true){
-    string start;
     REP(i, 8){
       if(! (cin >> num))
         return 0;
-      start += to_string(num);
+      start[i] = num;
     }
-    ans = solve(start);
+    int e = encode(start);
+    ans = solve(e);
     cout << ans << endl;
   }
   return 0;
