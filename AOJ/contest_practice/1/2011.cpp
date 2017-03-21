e3da485d8fde09f44dd9578d6972b688c2e91188
Rikuya6 <rikuya6@gmail.com>
2017-03-21 23:14:21 +0900

    途中

---
+++ AOJ/contest_practice/1/2011.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define REP(i, n) for(int i = 0; i < (int)(n); i++ )
+
+int main()
+{
+  int n;
+  while(cin >> n, n) {
+    
+  }
+}
