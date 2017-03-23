e1759a9e08660986e5dc8a52b3ee20112e3bda78
Rikuya6 <rikuya6@gmail.com>
2017-03-23 15:37:31 +0900

    TLE

--- AOJ/contest_practice/2/E.cpp
+++ AOJ/contest_practice/2/E.cpp
@@ -11,61 +11,27 @@ int main()
     int cnt = 0;
     int height = 0;
     int width = 0;
-    REP(i, N){
+    REP(i, N)
       cin >> h[i];
-    }
-    REP(i, M){
+    REP(i, M)
       cin >> w[i];
-    }
-
-    REP(i, M){
-      REP(k, N){
-        height = 0;
-        for(int u = k; u < N; u++){
-          height += h[u];
-          if(w[i] == height){
-            cnt++;
-            cout << " c1" << endl;
-            break;
-          }
-        }
-      }
-    }
-    //cout << cnt << endl;
-
     width = 0;
     REP(i, M){
-      width += w[i];
-      if(i == 0) continue;
-      REP(k, N){
-        height = 0;
-        for(int u = k; u < N; u++){
-          height += h[u];
-          //cout << "w[" << i << "]:" << w[i] << ", width:" << width << " height:" << height << endl;
-          if(width == height){
-            cnt++;
-            cout << " c2" << endl;
-            break;
-          }
+      for(int b = i; b < M; b++){
+        width = 0;
+        for(int k = i; k <= b; k++){
+          width += w[k];
         }
-      }
-    }
-    //cout << cnt << endl;
-
-    width = 0;
-    for(int i = M - 1; i > 0; i--){
-      width += w[i];
-      if(i == M - 1) continue;
-      //cout << " " << w[i] << endl;
-      REP(k, N){
-        height = 0;
-        for(int u = k; u < N; u++){
-          height += h[u];
-          //cout << "  h:" << height << endl;
-          if(width == height){
-            cnt++;
-            cout << " c3" << endl;
-            break;
+        REP(k, N){
+          height = 0;
+          for(int u = k; u < N; u++){
+            height += h[u];
+            //cout << "w[" << i << "]:" << w[i] << ", width:" << width << " height:" << height << endl;
+            if(width == height){
+              cnt++;
+              //cout << " c2" << endl;
+              break;
+            }
           }
         }
       }
