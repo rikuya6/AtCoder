dcceaaa9ea2eae3fc572e2204654017eed803dbc
Rikuya6 <rikuya6@gmail.com>
2017-03-28 23:53:49 +0900

    2011.cpp contest_practice1/D問題

--- AOJ/contest_practice/1/2011.cpp
+++ AOJ/contest_practice/1/2011.cpp
@@ -1,19 +1,63 @@
+// D問題
+
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 #define REP(i, n) for(int i = 0; i < (int)(n); i++ )
 
+int n;
+bool p_schedules[50][31];
+long long dp[50][31];
+
+int solve(const long long &goal)
+{
+  for(int day = 1; day < 31; day++) {
+    REP(own, n) {
+      if(p_schedules[own][day]) {
+        REP(other, n) {
+          if(p_schedules[other][day]) {
+            dp[own][day] |= dp[other][day - 1];
+          }
+        }
+      }else{
+        dp[own][day] |= dp[own][day - 1];
+      }
+      if(dp[own][day] == goal)
+        return day;
+    }
+  }
+  return -1;
+}
+
 int main()
 {
-  int n;
   while(cin >> n, n) {
-    int p_days[n][30];
+    REP(i, n)
+      REP(k, 31){
+        p_schedules[i][k] = false;
+        dp[i][k] = 0;
+      }
     REP(i, n) {
       int num_days;
       cin >> num_days;
-      REP(k, num_days)
-        cin >> p_days[n][k];
+      REP(k, num_days) {
+        int day;
+        cin >> day;
+        p_schedules[i][day] = true;
+      }
     }
+    const long long goal = (1LL << n) - 1; // n bitの最大値
+    // cout << goal << endl;
+    int ans_day = -1;
+    REP(i, n){
+      dp[i][0] |= 1LL << i;
+    }
+    ans_day = solve(goal);
+    if(ans_day != -1)
+      cout << ans_day << endl;
+    else
+      cout << "-1" << endl;
   }
+  return 0;
 }
