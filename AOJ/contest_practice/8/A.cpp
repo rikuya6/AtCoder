6c053cf0dcd7b5d0f77e1ba1319cb4507bd7af85
Rikuya6 <rikuya6@gmail.com>
2017-06-14 17:35:35 +0900

    練習

---
+++ AOJ/contest_practice/8/A.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  int n, m;
+  while(cin >> n >> m, n && m) {
+    int ncard[n], nsum = 0;
+    int mcard[m], msum = 0;
+    // int min_sum = INF;
+    REP(i, n) {
+      cin >> ncard[i];
+      nsum += ncard[i];
+    }
+    REP(i, m) {
+      cin >> mcard[i];
+      msum += mcard[i];
+    }
+    sort(ncard, ncard + n);
+    sort(mcard, mcard + m);
+    int new_nsum, new_msum;
+    bool check = false;
+    REP(i, n) {
+      if(check) break;
+      REP(k, m) {
+        new_nsum = nsum - ncard[i] + mcard[k];
+        new_msum = msum - mcard[k] + ncard[i];
+        if(new_nsum == new_msum) {
+          cout << ncard[i] << " " << mcard[k] << endl;
+          check = true;
+          break;
+        }
+      }
+    }
+    if(!check) cout << -1 << endl;
+  }
+}
---
+++ AOJ/contest_practice/8/B.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  vector<int> a;
+  vector<int> b;
+  vector<int> c;
+
+  for(int i = 2; i < 300000; i++){
+    if(i % 7 == 1 || i % 7 == 6){
+      a.push_back(i);
+    }
+  }
+
+  int len = a.size();
+  b.push_back(6);
+  for(int i = 1; i < len; i++){
+    bool flag = false;
+    int s = sqrt(i);
+    REP(k, s){
+      if(a[i] % a[k] == 0){
+        //cout << a[i] << " ! " << a[k] << endl;
+        flag = true;
+        break;
+      }
+    }
+    if(!(flag)) b.push_back(a[i]);
+  }
+
+  //cout << a[0] << endl;
+
+  int n;
+  while(cin >> n, n != 1){
+    cout << n << ":";
+    len = b.size();
+    REP(i, len){
+      if(n % b[i] == 0){
+        cout << " " << b[i];
+      }
+    }
+    cout << endl;
+  }
+
+
+  /*len = b.size();
+  REP(i, len){
+    cout << b[i] << " ";
+  }*/
+}
