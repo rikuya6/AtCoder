033fe9e5c1104eab994e152421d3eb238c8a153f
rikuya6 <rikuya6@gmail.com>
2018-02-25 21:37:29 +0900

    練習

--- AtCoder/ABC075/B.cpp
+++ AtCoder/ABC075/B.cpp
@@ -1,36 +1,33 @@
-// 未完成
-
 #include <iostream>
+#include <string>
 using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define REP(i, n) FOR(i, 0, n)
 
 int H, W;
-int dx[] = {1,  1 -1  -1, 0, 1, -1, 0};
-int dy[] = {1, -1, 1, -1, 1, 0,  0, -1};
-char s[50][50];
+// 右, 左, 上, 下　右上, 左上, 右下, 左下
+int dx[] = {1, -1,  0,  0, 1, -1, 1, -1};
+int dy[] = {0,  0, -1,  1, 1,  1, -1, -1};
+string s[50];
 int ret[50][50];
 
 void solve()
 {
-  FOR (i, 0, H) {
-    FOR (k, 0, W) {
+  REP (i, H) {
+    REP(k, W) {
       if (s[i][k] == '#')
         continue;
       REP(m, 8) {
-        if (i + m < 0 || k + m < 0 || i + m >= H || k + m >= W)
+        if (i + dy[m] < 0 || k + dx[m] < 0 || i + dy[m] >= H || k + dx[m] >= W)
           continue;
-        if (s[i + dy[m]][k + dx[m]] != '.' || s[i + dy[m]][k + dx[m]] != '#')
-          cout << s[i + dy[m]][k + dx[m]];
         if (s[i + dy[m]][k + dx[m]] == '#') {
           ret[i][k]++;
         }
       }
     }
-    cout << endl;
   }
-  FOR (i, 0, H) {
-    FOR (k, 0, W) {
+  REP (i, H) {
+    REP (k, W) {
       if (s[i][k] == '#')
         cout << "#";
       else
@@ -43,9 +40,7 @@ void solve()
 int main()
 {
   cin >> H >> W;
-  fill(s[0], s[50], '.');
-  FOR (i, 0, H)
-    FOR (k, 0, W)
-      cin >> s[i][k];
+  REP (i, H)
+    cin >> s[i];
   solve();
 }
