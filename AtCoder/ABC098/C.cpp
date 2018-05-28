52130b30ef777fa3d4bb784bfebacc8fe6caf699
rikuya6 <rikuya6@gmail.com>
2018-05-28 17:44:26 +0900

    練習

--- AtCoder/ABC098/C.cpp
+++ AtCoder/ABC098/C.cpp
@@ -5,12 +5,14 @@ using namespace std;
 #define FOR(i, a, b) for (size_t i = (a); i < (b); ++i)
 #define REP(i, n) FOR(i, 0, n)
 
-const int MAX_V = 100001;
+const int MAX_V = 2000001;
 int w_table[MAX_V];
 int e_table[MAX_V];
 
 void calc_table(const int& n, const string& s)
 {
+    w_table[0] = 0;
+    e_table[0] = 0;
     for (size_t i = 1; i <= n; ++i)
         if (s[i - 1] == 'W') {
             w_table[i] = w_table[i - 1] + 1;
@@ -20,27 +22,15 @@ void calc_table(const int& n, const string& s)
             e_table[i] = e_table[i - 1] + 1;
             w_table[i] = w_table[i - 1];
         }
-    }
 }
 
 int solve(const int n, string s)
 {
-    int min_counter = 1 << 30;
-    REP(i, n) {
-        int count = 0;
-        count = w_table[i + 1] + e_table[i + 1] - 1;
-        // if (s[i] == 'W') {
-        //     count += w_table[i];
-        //     count += e_table[n - 1] - e_table[i + 1];
-        // } else {
-        //     count += e_table[i] - e_table[i];
-        //     count += w_table[n]; //  - w_table[i + 1];
-        // }
-        if (count == 0) continue;
-        cout << count << endl;
-        min_counter = min(min_counter, count);
+    int count = 1;
+    FOR(i, 1, n + 1) {
+        count = min(count, w_table[i - 1] + e_table[n] - e_table[i]);
     }
-    return min_counter;
+    return count;
 }
 
 int main()
@@ -49,12 +39,12 @@ int main()
     string s;
     cin >> n >> s;
     calc_table(n, s);
-    REP(i, n + 1)
-        cout << w_table[i] << " ";
-    cout << endl;
-    REP(i, n + 1)
-        cout << e_table[i] << " ";
-    cout << endl;
+    // REP(i, n + 1)
+    //     cout << w_table[i] << " ";
+    // cout << endl;
+    // REP(i, n + 1)
+    //     cout << e_table[i] << " ";
+    // cout << endl;
     cout << solve(n, s) << endl;
 }
 
