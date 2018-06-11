c7cb354017fef3ac1b0ac78670f3aefc6a316b6f
rikuya6 <rikuya6@gmail.com>
2018-06-11 13:04:18 +0900

    練習

--- AtCoder/ABC099/C.cpp
+++ AtCoder/ABC099/C.cpp
@@ -2,39 +2,33 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FOR(i, a, b) for (size_t i = (a); i < (b); ++i)
 #define REP(i, n) FOR(i, 0, n)
 
-int solve(int n) {
-  vector<int> a, b;
-  a.push_back(1);
-  b.push_back(1);
-  for (auto i = 6; i <= 100000; i *= 6)
-    a.push_back(i);
-  for (auto i = 9; i <= 100000; i *= 9)
-    b.push_back(i);
-  int cnt {};
-  while (n) {
-    auto a_itr = upper_bound(a.begin(), a.end(), n);
-    auto b_itr = upper_bound(b.begin(), b.end(), n);
-    // cout << *(b_itr - 1) << endl;
-    // cout << *(a_itr - 1) << endl;
-    // if (n == 1) {
-    //   cout << *(b_itr) << endl;
-    //   cout << *(a_itr) << endl;
-    //   // return -1;
-    // }
-    if (*(a_itr - 1) <= *(b_itr - 1)) {
-      n -= *(b_itr - 1);
-    } else {//if (*(a_itr - 1) > *(b_itr - 1)) {
-      n -= *(a_itr - 1);
-    } //else {
-      // --n;
-    // }
-    // cout << n << endl;
-    ++cnt;
-  }
+int dp[100000 + 1][100];
 
+int solve(int n) {
+  vector<int> coins(1, 1);
+  for (auto i = 6; i <= n; i *= 6)
+    coins.push_back(i);
+  for (auto i = 9; i <= n; i *= 9)
+    coins.push_back(i);
+  sort(coins.begin(), coins.end());
+  REP(i, coins.size())
+    dp[0][i] = 0;
+  FOR(i, 1, n + 1)
+    dp[i][0] = i;
+  FOR(i, 1, n + 1) {
+    FOR(k, 1, coins.size()) {
+      dp[i][k] = dp[i][k - 1];
+      if (i >= coins[k]) {
+        dp[i][k] = min(dp[i][k], 1 + dp[i - coins[k]][k]);
+      }
+    }
+  }
+  int cnt = n;
+  REP(i, coins.size())
+    cnt = min(cnt, dp[n][i]);
   return cnt;
 }
 
