6a3d067ad6bbafd78783c5a52ab7b312085523e5
rikuya6 <rikuya6@gmail.com>
2018-07-02 11:49:13 +0900

    練習

--- AtCoder/ABC043/C.cpp
+++ AtCoder/ABC043/C.cpp
@@ -5,5 +5,22 @@ using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define REP(i, n) FOR(i, 0, n)
 
+int solve(const vector<int>& a, const int n) {
+  int ans = 1 << 30;
+  for (int i = -100; i <= 100; ++i) {
+    int sum {};
+    for (auto e : a) {
+      sum += (e - i) * (e - i);
+    }
+    ans = min(ans, sum);
+  }
+  return ans;
+}
+
 int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  REP(i, n) cin >> a[i];
+  cout << solve(a, n) << endl;
 }
